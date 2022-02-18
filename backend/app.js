619da5bdd6aacdff74cac7b0093ef968f11d5d11
Prafulla Pal <44419039+prafullapal@users.noreply.github.com>
2022-02-19 04:46:14 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,100 @@
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+const cors = require("cors");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, x-access-token, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+const db = require("./models");
+const req = require("express/lib/request");
+console.log(db);
+const Role = db.role;
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    keepAlive: true,
+  })
+  .then(() => {
+    console.log("connection established to DB");
+    initial();
+  })
+  .catch((error) => {
+    console.log("connection failed", error);
+  });
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'user' to roles collection");
+      });
+      new Role({
+        name: "moderator",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'moderator' to roles collection");
+      });
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ backend/models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const db = {};
+db.user = require("./user");
+db.role = require("./role");
+
+db.ROLES = ["user", "admin", "moderator"];
+
+
+module.exports = db;
